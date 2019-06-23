6a5823dc14e5ff1dc9235b6c8569a9c758b6ba13
nealxu <xuqixq@sina.com>
2019-06-23 21:12:00 +0800

    init e6.11

---
+++ src/main/scala/io/neal/chapter06/Machine.scala
@@ -0,0 +1,39 @@
+package io.neal.chapter06
+
+
+/**
+ * e6.11
+ * Hard: To gain experience with the use of State, implement a finite state automaton
+ * that models a simple candy dispenser. The machine has two types of input: you can
+ * insert a coin, or you can turn the knob to dispense candy. It can be in one of two
+ * states: locked or unlocked. It also tracks how many candies are left and how many
+ * coins it contains.
+ */
+sealed trait Input
+
+case object Coin extends Input
+
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Machine {
+
+  import State._
+
+  private var machine: Option[Machine] = None
+
+  def apply(locked: Boolean, candies: Int, coins: Int): Machine = {
+    machine.getOrElse {
+      val m = new Machine(locked, candies, coins)
+      machine = Some(m)
+      m
+    }
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    unit((0, 0))
+  }
+
+}
+
---
+++ src/test/scala/io/neal/chapter06/MachineTest.scala
@@ -0,0 +1,8 @@
+package io.neal.chapter06
+
+import org.scalatest.FlatSpec
+
+class MachineTest extends FlatSpec {
+
+
+}
